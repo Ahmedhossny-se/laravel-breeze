00fa20c33ce66ae846a3eccee43071a226b4e5d1
Ahmed hossny sobhy <82140094+Ahmedhossny-se@users.noreply.github.com>
2022-12-19 23:15:02 +0200

    Add files via upload

---
+++ storage/framework/views/2ebea3d6a3568cce9b03b32a5cebc261b6449c87.php
@@ -0,0 +1,8 @@
+<div class="relative text-gray-500 focus-within:text-gray-900 dark:focus-within:text-gray-400">
+    <div aria-hidden="true" class="absolute inset-y-0 flex items-center px-4 pointer-events-none">
+        <?php echo e($icon); ?>
+
+    </div>
+    <?php echo e($slot); ?>
+
+</div><?php /**PATH D:\MSA\SERVICE-ORIENTED\laravel-app\resources\views/components/input-with-icon-wrapper.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/bcb587a13715fb75f3612bb28c0d5ca6bcadaa1e.php
@@ -0,0 +1,26 @@
+<footer class="flex-shrink-0 px-6 py-4">
+    <p class="flex items-center justify-center gap-1 text-sm text-gray-600 dark:text-gray-400">
+        <span>Made with</span>
+        <span>
+            <?php if (isset($component)) { $__componentOriginalcd9972c8156dfa6e5fd36675ca7bf5f21b506e2e = $component; } ?>
+<?php $component = BladeUI\Icons\Components\Svg::resolve([] + (isset($attributes) ? (array) $attributes->getIterator() : [])); ?>
+<?php $component->withName('heroicon-s-heart'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $constructor = (new ReflectionClass(BladeUI\Icons\Components\Svg::class))->getConstructor()): ?>
+<?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['class' => 'w-6 h-6 text-red-500']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalcd9972c8156dfa6e5fd36675ca7bf5f21b506e2e)): ?>
+<?php $component = $__componentOriginalcd9972c8156dfa6e5fd36675ca7bf5f21b506e2e; ?>
+<?php unset($__componentOriginalcd9972c8156dfa6e5fd36675ca7bf5f21b506e2e); ?>
+<?php endif; ?>
+        </span>
+        <span>by</span>
+        <a href="https://github.com/Kamona-WD" target="_blank" class="text-blue-600 hover:underline">
+            Ahmed Kamel
+        </a>
+    </p>
+</footer><?php /**PATH D:\MSA\SERVICE-ORIENTED\laravel-app\resources\views/components/footer.blade.php ENDPATH**/ ?>
